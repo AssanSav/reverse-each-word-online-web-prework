6fbc2372622933a226382ce6c670c02125ce1116
assane savadogo <assane.savadogo81@gmail.com>
2019-06-02 02:00:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,3 @@ def reverse_each_word(string)
   return nuarray.join(" ")
 end
 
-def reverse_each_word(string)
-  array = string.split(" ") 
-  test_array = []
-  array.collect do|string| 
-    test_array << string.reverse 
-  end
-  test_array.join(" ")
-end
