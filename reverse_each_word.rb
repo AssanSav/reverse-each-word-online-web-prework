91e921d8877ca1e15e1d34bb917435cdb318b2c2
assane savadogo <assane.savadogo81@gmail.com>
2019-06-02 01:41:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence1)
-  nuarray = [].reverse
-  sentence1.each do |word|
+def reverse_each_word(array)
+  nuarray = []
+  array.each do |word|
     nuarray << word
   end
   return nuarray
